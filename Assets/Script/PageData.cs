58d45987346abfb4c5dd257f92f722e8de981176
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2023-09-26 10:40:32 +0900

    캐릭터, 박스 Prefab화 완료

---
+++ Assets/Script/PageData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PageData", menuName = "Scriptable Object/Page Data", order = int.MaxValue)]
+
+public class PageData : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
