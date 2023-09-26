0630fee7e2353ed5687d8a0cb11e779dbf269111
김은서 <a55208806@gmail.com>
2023-09-26 10:34:14 +0900

    스위치와 움직이는 벽 구현

---
+++ Assets/Script/Wall.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private float startPositionY;
+    [SerializeField] private float moveDistance;
+    private float endPositionY;
+
+    private void Start()
+    {
+        startPositionY = transform.position.y;
+        endPositionY = startPositionY + moveDistance;
+    }
+    public void Move()
+    {
+        transform.Translate(0, speed, 0);
+        if (transform.position.y >endPositionY)
+        {
+            transform.position = new Vector3(transform.position.x, endPositionY, 0);
+        }
+    }
+
+
+    public void ReverseMove()
+    {
+        transform.Translate(0, -speed, 0);
+        if (transform.position.y < startPositionY)
+        {
+            transform.position = new Vector3(transform.position.x, startPositionY, 0);
+        }
+    }
+    private void OnDisable()
+    {
+        transform.position = new Vector3(transform.position.x, startPositionY, 0);
+    }
+}
